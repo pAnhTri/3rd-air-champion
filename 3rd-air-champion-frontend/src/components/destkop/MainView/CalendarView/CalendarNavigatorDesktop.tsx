eddb8733c967f4c62eff496cff96aab346cad984
Anh Tri Pham <anhtp5@uci.edu>
2025-01-09 19:12:44 -0800

    fix: revert changes, fixed UI for occupancy

--- 3rd-air-champion-frontend/src/components/destkop/MainView/CalendarView/CalendarNavigatorDesktop.tsx
+++ 3rd-air-champion-frontend/src/components/destkop/MainView/CalendarView/CalendarNavigatorDesktop.tsx
@@ -27,50 +27,40 @@ const CalendarNavigator = ({
       {/* Date */}
       <div className="flex h-full w-full justify-center items-center space-x-2">
         <span className="font-bold text-xl text-gray-800">{formattedDate}</span>
-        {showDetails ? (
-          <div
-            onClick={() => setShowDetails(false)}
-            className="flex cursor-pointer space-x-2"
-          >
-            {occupancy.roomOccupancy
-              .filter((room) => room.name !== "Master") // Exclude "Master"
-              .map((object, index) => {
-                // Determine the color class based on occupancy
-                const occupancyColor =
-                  object.occupancy < 33.33
-                    ? "text-red-500"
-                    : object.occupancy < 66.67
-                    ? "text-yellow-500"
-                    : "text-green-500";
+      </div>
+      {showDetails ? (
+        <div
+          onClick={() => setShowDetails(false)}
+          className="flex h-full w-full justify-center items-center cursor-pointer space-x-2"
+        >
+          {occupancy.roomOccupancy
+            .filter((room) => room.name !== "Master") // Exclude "Master"
+            .map((object, index) => {
+              // Determine the color class based on occupancy
+              const occupancyColor =
+                object.occupancy < 33.33
+                  ? "text-red-500"
+                  : object.occupancy < 66.67
+                  ? "text-yellow-500"
+                  : "text-green-500";
 
-                return (
-                  <div key={index} className="flex space-x-1 w-full">
-                    <span className="font-medium">{object.name}: </span>
-                    <span className={occupancyColor}>
-                      {Math.round(object.occupancy)}%
-                    </span>
-                  </div>
-                );
-              })}
-            <div className="flex space-x-1 w-full">
-              <span className="font-medium">AirBnB: </span>
-              <span
-                className={`${
-                  occupancy.airbnbOccupancy < 33.33
-                    ? "text-red-500"
-                    : occupancy.airbnbOccupancy < 66.67
-                    ? "text-yellow-500"
-                    : "text-green-500"
-                }`}
-              >
-                {Math.round(occupancy.airbnbOccupancy)}%
-              </span>
-            </div>
-          </div>
-        ) : (
+              return (
+                <div key={index} className="space-x-1">
+                  <span className="font-medium">{object.name}: </span>
+                  <span className={occupancyColor}>
+                    {Math.round(object.occupancy)}%
+                  </span>
+                </div>
+              );
+            })}
+        </div>
+      ) : (
+        <div
+          className="flex h-full w-full justify-center items-center cursor-pointer space-x-2"
+          onClick={() => setShowDetails(true)}
+        >
           <span
-            onClick={() => setShowDetails(true)}
-            className={`cursor-pointer underline ${
+            className={`cursor-pointer flex underline ${
               occupancy.totalOccupancy < 33.33
                 ? "text-red-500"
                 : occupancy.totalOccupancy < 66.67
@@ -78,10 +68,21 @@ const CalendarNavigator = ({
                 : "text-green-500"
             }`}
           >
-            {Math.round(occupancy.totalOccupancy)}%
+            {Math.round(occupancy.totalOccupancy)}% occupancy
           </span>
-        )}
-      </div>
+          <span
+            className={`underline ${
+              occupancy.airbnbOccupancy < 33.33
+                ? "text-red-500"
+                : occupancy.airbnbOccupancy < 66.67
+                ? "text-yellow-500"
+                : "text-green-500"
+            }`}
+          >
+            {Math.round(occupancy.airbnbOccupancy)}% (A)booking
+          </span>
+        </div>
+      )}
 
       {/* Bottom Section: Days of the Week */}
       <div className="grid grid-cols-7 text-center">
